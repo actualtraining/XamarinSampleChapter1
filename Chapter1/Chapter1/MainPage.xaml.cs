edb2c52b1035c70118541a9b0c0d0ce079fbf498
Erick Kurniawan <erick.kurniawan@gmail.com>
2018-01-16 15:59:25 +0700

    Add project files.

---
+++ Chapter1/Chapter1/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Chapter1
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            btnNew.Clicked += BtnNew_Clicked;
+            
+		}
+
+        private void BtnSave_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Keterangan", "Anda memilih tombol Save", "OK");
+        }
+
+        private void BtnNew_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Keterangan", "Hello Xamarin Forms", "OK");
+        }
+    }
+}
