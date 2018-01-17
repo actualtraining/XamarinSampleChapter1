d5c5e6d2613d42abed23d6a38dcb0668f9cef8d4
Erick Kurniawan <erick.kurniawan@gmail.com>
2018-01-17 13:53:57 +0700

    menambahkan data sample

--- Chapter1/Chapter1/ViewModels/DaftarMenuViewModel.cs
+++ Chapter1/Chapter1/ViewModels/DaftarMenuViewModel.cs
@@ -2,11 +2,30 @@
 using System.Collections.Generic;
 using System.Text;
 using Xamarin.Forms;
+using Chapter1.Models;
+
 
 namespace Chapter1.ViewModels
 {
     public class DaftarMenuViewModel : BindableObject
     {
+        private List<DaftarMenu> listDaftarMenu;
+        public List<DaftarMenu> ListDaftarMenu
+        {
+            get { return listDaftarMenu; }
+            set { listDaftarMenu = value; OnPropertyChanged("ListDaftarMenu"); }
+        }
 
+        public DaftarMenuViewModel()
+        {
+            ListDaftarMenu = new List<DaftarMenu>
+            {
+                new DaftarMenu {Nama="Sate Klathak",Keterangan="Sate khas jogja ditusuk besi"},
+                new DaftarMenu {Nama="Gudeg Yu Djum",Keterangan="Gudeg paling terkenal di jogja"},
+                new DaftarMenu {Nama="Nasi Goreng Kambing",Keterangan="Nasi goreng dengan daging kambing masak rempah"}
+            };
+        }
+
+       
     }
 }
